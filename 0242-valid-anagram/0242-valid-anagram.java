8077d34e3ab851ab3844c26a0e4e7c5ad7c330b3
Jagadeesh <37505639+kjagadeesh683@users.noreply.github.com>
2026-03-09 18:55:25 -0400

    0242-valid-anagram

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+        // Approach 1:
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
+        // Map<Character, Integer> map = new HashMap<>();
+        // for (char c : s.toCharArray()) {
+        //     map.put(c, map.getOrDefault(c, 0) + 1);
+        // }
+        // for (char c : t.toCharArray()) {
+        //     map.put(c, map.getOrDefault(c, 0) - 1);
+        // }
+        // for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+        //     if (entry.getValue() != 0) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        //Approach 2:
         if (s.length() != t.length()) {
             return false;
         }
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
-        for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) - 1);
-        }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue() != 0) {
+
+        for (int i : freq) {
+            if (i != 0) {
                 return false;
             }
         }
