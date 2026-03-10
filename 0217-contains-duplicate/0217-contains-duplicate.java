fbd7cfe44ed57ba8cd540e0e6be6a68c4c8eb64f
Jagadeesh <37505639+kjagadeesh683@users.noreply.github.com>
2026-03-09 23:12:51 -0400

    0217-contains-duplicate

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,20 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> result = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (result.containsKey(nums[i])) {
+        // Map<Integer, Integer> result = new HashMap<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (result.containsKey(nums[i])) {
+        //         return true;
+        //     }
+        //     result.put(nums[i], i);
+        // }
+        // return false;
+
+        Set<Integer> seen = new HashSet<>();
+        for (int n : nums) {
+            if (seen.contains(n)) {
                 return true;
             }
-            result.put(nums[i], i);
+            seen.add(n);
         }
         return false;
     }
