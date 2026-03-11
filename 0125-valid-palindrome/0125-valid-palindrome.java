e91d1f8418547b01d22c08554dc094af1860297b
Jagadeesh <37505639+kjagadeesh683@users.noreply.github.com>
2026-03-10 23:23:35 -0400

    0125-valid-palindrome

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder newStr = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                newStr.append(Character.toLowerCase(c));
+        // Approach 1:
+        // StringBuilder newStr = new StringBuilder();
+        // for (char c : s.toCharArray()) {
+        //     if (Character.isLetterOrDigit(c)) {
+        //         newStr.append(Character.toLowerCase(c));
+        //     }
+        // }
+        // return newStr.toString().equals(newStr.reverse().toString());
+
+        // Approach 2:
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
             }
+            while (right > left && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
         }
-        return newStr.toString().equals(newStr.reverse().toString());
+        return true;
     }
 }
