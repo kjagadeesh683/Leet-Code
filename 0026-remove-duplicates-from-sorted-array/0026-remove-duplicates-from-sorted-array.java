56f30c7c65f54a422589582fa6e5e82e165a205f
Jagadeesh <37505639+kjagadeesh683@users.noreply.github.com>
2026-03-11 00:42:07 -0400

    0026-remove-duplicates-from-sorted-array

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,13 +1,23 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-               Set<Integer> unique = new TreeSet<>();
-        for (int num : nums) {
-            unique.add(num);
+        // Approach 1:
+        // Set<Integer> unique = new TreeSet<>();
+        // for (int num : nums) {
+        //     unique.add(num);
+        // }
+        // int i = 0;
+        // for (int num : unique) {
+        //     nums[i++] = num;
+        // }
+        // return unique.size();
+
+        //Approach 2:
+        int l = 1;
+        for (int r = 1; r < nums.length; r++) {
+            if (nums[r] != nums[r - 1]) {
+                nums[l++] = nums[r];
+            }
         }
-        int i = 0;
-        for (int num : unique) {
-            nums[i++] = num;
-        }
-        return unique.size();
+        return l;
     }
 }
